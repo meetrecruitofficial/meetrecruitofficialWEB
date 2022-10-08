c37e97e38b6afbc9bcd7a001633bc92ebd82a950
Ekete Emmanuel <eketeeu@gmail.com>
2022-10-08 07:11:31 -0700

    login api

--- signUp/countries.js
+++ signUp/countries.js
@@ -298,7 +298,7 @@ function populateCountries(countryElementId, stateElementId){
 }
 
 
-// post login form using fetch API
+// post signUp form using fetch API
 const signupForm = document.getElementById('signUp')
 
 signupForm.addEventListener('submit', async function (evt) {
@@ -318,6 +318,66 @@ signupForm.addEventListener('submit', async function (evt) {
    
 
     await fetch('https://meetrecruitofficial.cyclic.app/api/register',{
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            fname: fname,
+            lname: lname,
+            phone: phone,
+            sex: sex,
+            age: '50',
+            country: country,
+            skills :'Developer',
+            level :skillLevel,
+            state : state,
+            email: email,
+            password : password,
+        })
+    })
+        .then(res => res.json())
+        .then(data => {
+            const message = data.message;
+            const token = data.token;
+            const error = data.error;
+            if (message) {
+                swal({
+                    icon: "error",
+                    text: message ,
+                    button: false,
+                })
+            } else if (error) {
+                swal({
+                    icon: "error",
+                    text: error,
+                    button: false,
+                })
+            }
+            else {
+                localStorage.setItem('token',token)
+                swal({
+                    icon: "success",
+                    text: "Sign-Up successful",
+                    button: false,
+                })
+                setTimeout(() => { location.assign('/'); }, 1700);
+                // setTimeout(() => { location.assign('/index'); }, 1000);
+            }
+        })
+
+})
+
+
+//// login api
+const loginForm = document.getElementById('login')
+
+loginForm.addEventListener('submit', async function (evt) {
+    evt.preventDefault(); // to prevent the custom form property of reloading on submission.
+
+    const email = document.getElementById('login-email').value;
+    const password = document.getElementById('login-password').value;
+   
+
+    await fetch('https://meetrecruitofficial.cyclic.app/api/sign-in',{
         method: 'POST',
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
